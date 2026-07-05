fe55753617d7b3a2548ec307576a1ef737c90a34
Lucas Medeiros <lucasmedeiroscosta07@gmail.com>
2026-07-05 17:47:17 -0300

    chore: add Husky pre-commit (lint-staged+Prettier) and pre-push (tsc+lint)

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
-const expoConfig = require("eslint-config-expo/flat");
+const expoConfig = require('eslint-config-expo/flat');
+const prettier = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   {
     ignores: [
-      "dist/*",
-      ".agents/**",
-      ".superpowers/**",
-      "design-completo-foodfight-game/**",
-      "node_modules/**"
+      'dist/*',
+      '.agents/**',
+      '.superpowers/**',
+      'design-completo-foodfight-game/**',
+      'node_modules/**',
     ],
   },
   expoConfig,
   {
     rules: {
-      "react-hooks/immutability": "off",
-      "react-hooks/refs": "off",
-      "import/no-named-as-default": "off",
-    }
-  }
+      'react-hooks/immutability': 'off',
+      'react-hooks/refs': 'off',
+      'import/no-named-as-default': 'off',
+    },
+  },
+  prettier,
 ]);
